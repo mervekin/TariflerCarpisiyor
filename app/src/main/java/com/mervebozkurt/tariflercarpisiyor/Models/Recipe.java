22df4b193b6e532c119c06e1622be334e43da4ec
mervekin <mervebozkurt1998@gmail.com>
2021-05-16 01:20:12 +0300

    16.05.2021

---
+++ app/src/main/java/com/mervebozkurt/tariflercarpisiyor/Models/Recipe.java
@@ -0,0 +1,153 @@
+package com.mervebozkurt.tariflercarpisiyor.Models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.firestore.FieldValue;
+
+import java.util.Date;
+import java.util.List;
+
+public class Recipe {
+    public String documentId;
+    public String useremail;
+    public String username;
+    public String category;
+    public String mealname;
+    public String cookingstep;
+    public String cookingtime;
+    public String mealportion;
+    public String downloadUrl;
+    public Float mealrating;
+    public String Userid;
+    public FieldValue date;
+
+    List<String> ingredientlist;
+
+    public Recipe(){
+
+    }
+    public Recipe(String Userid,String documentId, String mealname,String downloadUrl) {
+        this.Userid=Userid;
+        this.category = category;
+        this.mealname = mealname;
+        this.downloadUrl = downloadUrl;
+    }
+    public Recipe(String documentId ,String Userid,String useremail, String username, String category, String mealname, String cookingstep, String cookingtime, String mealportion, String downloadUrl, List<String> ingredientlist, FieldValue date) {
+        this.documentId=documentId;
+        this.Userid=Userid;
+        this.useremail = useremail;
+        this.username = username;
+        this.category = category;
+        this.mealname = mealname;
+        this.cookingstep = cookingstep;
+        this.cookingtime = cookingtime;
+        this.mealportion = mealportion;
+        this.downloadUrl = downloadUrl;
+        this.ingredientlist = ingredientlist;
+        this.date=date;
+    }
+    public Recipe(Float mealrating){
+        this.mealrating=mealrating;
+
+    }
+
+
+    public Float getMealrating() {
+        return mealrating;
+    }
+
+    public void setMealrating(Float mealrating) {
+        this.mealrating = mealrating;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public void setUseremail(String useremail) {
+        this.useremail = useremail;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setMealname(String mealname) {
+        this.mealname = mealname;
+    }
+
+    public void setCookingstep(String cookingstep) {
+        this.cookingstep = cookingstep;
+    }
+
+    public void setCookingtime(String cookingtime) {
+        this.cookingtime = cookingtime;
+    }
+
+    public void setMealportion(String mealportion) {
+        this.mealportion = mealportion;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+
+    public void setDate(FieldValue date) {
+        this.date = date;
+    }
+
+    public void setIngredientlist(List<String> ingredientlist) {
+        this.ingredientlist = ingredientlist;
+    }
+
+
+
+    public String getUseremail() {
+        return useremail;
+    }
+
+    public FieldValue getDate() {
+        return date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getMealname() {
+        return mealname;
+    }
+
+    public String getCookingstep() {
+        return cookingstep;
+    }
+
+    public String getCookingtime() {
+        return cookingtime;
+    }
+
+    public String getMealportion() {
+        return mealportion;
+    }
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public List<String> getIngredientlist() {
+        return ingredientlist;
+    }
+
+}
---
+++ app/src/main/java/com/mervebozkurt/tariflercarpisiyor/RecipeViewHolder.java
@@ -0,0 +1,35 @@
+package com.mervebozkurt.tariflercarpisiyor;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView MealImage;
+    public TextView MealName,UserName,MealPortion,MealScore,MealTime;
+    public ImageView DeleteRecipe,UpdateRecipe;
+    public ProgressBar progressBar;
+
+
+
+    public RecipeViewHolder(@NonNull View itemView) {
+        super(itemView);
+        UserName=itemView.findViewById(R.id.recyclerview_row_user_name);
+        MealImage=itemView.findViewById(R.id.recyclerview_row_meal_imageView);
+        MealName=itemView.findViewById(R.id.recyclerview_row_meal_name);
+        MealPortion=itemView.findViewById(R.id.recyclerview_row_meal_portion);
+        MealScore=itemView.findViewById(R.id.recyclerview_row_meal_score);
+        MealTime=itemView.findViewById(R.id.recyclerview_row_meal_time);
+        DeleteRecipe=itemView.findViewById(R.id.btn_delete);
+        UpdateRecipe=itemView.findViewById(R.id.btn_update);
+
+
+        progressBar=itemView.findViewById(R.id.progressBar);
+
+    }
+}
